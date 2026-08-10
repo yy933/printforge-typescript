d2fe05b636d051d29a8d2e52e92fee1bdad9ea4a
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-10 18:48:54 +0800

    feat: add link on 3d-models page navigating to details page of each model

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,6 +1,11 @@
+import Link from "next/link";
 import { getAllModels } from "@/app/lib/models";
 import type { Model } from "@/app/types";
 export default async function ModelsPage() {
   const models = await getAllModels();
-  return models.map((model: Model) => <p key={model.id}>{model.name}</p>);
+  return models.map((model: Model) => (
+    <Link href={`/3d-models/${model.id.toString()}`}>
+      <p key={model.id}>{model.name}</p>
+    </Link>
+  ));
 }
