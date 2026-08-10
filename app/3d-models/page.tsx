932f23c5b26620857f203b9a7efd95a11b638663
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-10 18:14:09 +0800

    feat: fetch and map over model data, and display a list of model names

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,3 +1,6 @@
-export default function ModelsPage() {
-  return <h1>Models</h1>;
+import { getAllModels } from "@/app/lib/models";
+import type { Model } from "@/app/types";
+export default async function ModelsPage() {
+  const models = await getAllModels();
+  return models.map((model: Model) => <p key={model.id}>{model.name}</p>);
 }
