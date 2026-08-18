0489fdd52f1e02765c5ea20bf6ee21b5907fc0f6
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-18 17:28:31 +0800

    feat: add isActive status to sort buttons

--- app/components/SortButton.tsx
+++ app/components/SortButton.tsx
@@ -1,13 +1,27 @@
+"use client";
 import React from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 interface SortButtonProps {
+  sort: string;
   children: React.ReactNode;
 }
-export default function SortButton({ children }: SortButtonProps) {
+export default function SortButton({
+  sort = "alpha",
+  children,
+}: SortButtonProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const isActive = searchParams.get("sort") === sort;
+  function handleSort() {
+    const url = `${pathname}?sort=${sort}`;
+    router.push(url);
+  }
   return (
     <button
-      className={
-        "px-3 py-1.5 text-sm rounded-full border cursor-pointer border-gray-300 text-gray-700 hover:bg-gray-100"
-      }
+      onClick={handleSort}
+      className={`px-3 py-1.5 text-sm rounded-full border cursor-pointer border-gray-300 text-gray-700 hover:bg-gray-100 hover:text-gray-800 ${isActive ? "text-white bg-orange-400 border-orange-400" : "border-gray-300 text-gray-700 hover:bg-gray-100"}`}
     >
       {children}
     </button>
--- app/components/SortControls.tsx
+++ app/components/SortControls.tsx
@@ -1,11 +1,30 @@
 import SortButton from "./SortButton";
+/*  
+CHALLENGE  
+Update the buttons so each one pushes a different sort value:
+
+- A-Z → sort=alpha  
+- Popular → sort=popular  
+- Most Recent → sort=recent  
+
+You can pass the sort value into your handler however you like
+*/
+
+
 export default function SortControls() {
+  const sortMap = {
+    alpha: "A-Z",
+    popular: "Popular",
+    recent: "Most Recent",
+  }
   return (
     <div className="flex items-center gap-2">
       <span className="text-sm text-gray-600">Sort by:</span>
-      <SortButton>A-Z</SortButton>
-      <SortButton>Popular</SortButton>
-      <SortButton>Most Recent</SortButton>
+      {Object.entries(sortMap).map(([sort, label]) => (
+        <SortButton key={sort} sort={sort}>
+          {label}
+        </SortButton>
+      ))}
     </div>
   );
 }
