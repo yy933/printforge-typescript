9ef4891312c1e80b44b7debfe752b53d3cfa02b3
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-18 17:06:33 +0800

    feat: add SortControls compontnt into ModelsGrid and update ModelsGrid style

--- app/components/ModelsGrid.tsx
+++ app/components/ModelsGrid.tsx
@@ -1,4 +1,5 @@
 import ModelCard from "@/app/components/ModelCard";
+import SortControls from "@/app/components/SortControls";
 import { ModelsGridProps, Model } from "@/app/types";
 
 export default function ModelsGrid({
@@ -14,7 +15,10 @@ export default function ModelsGrid({
   }
   return (
     <div className="container px-4 py-8 mx-auto">
-      <h1 className="mb-8 text-3xl font-bold">{title}</h1>
+      <div className="flex flex-col gap-2 md:flex-row md:justify-between mb-8">
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <SortControls />
+      </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {models.map((model: Model) => (
           <ModelCard key={model.id} model={model} />
--- app/components/SortControls.tsx
+++ app/components/SortControls.tsx
@@ -1,10 +1,3 @@
-/*  
-CHALLENGE  
-- Create a <SortButton /> component  
-- Move the existing <button> into that component  
-- Use `children` so each button can display different text  
-- Replace the three buttons with your new <SortButton /> component  
-*/
 import SortButton from "./SortButton";
 export default function SortControls() {
   return (
