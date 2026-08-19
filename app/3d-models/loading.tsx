c9ce40b87544d4bac06e15543e4a572a57641aa8
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-19 19:19:23 +0800

    refactor: extract laodingUI from /3d-models/loading.tsx

--- app/3d-models/loading.tsx
+++ app/3d-models/loading.tsx
@@ -1,17 +1,4 @@
-import Image from "next/image";
+import LoadingUI from "../components/LoadingUI";
 export default function ModelsLoading() {
-  return (
-    <main>
-      <h1 className="font-bold text-2xl text-orange-400 text-center mb-4">
-        Loading models...
-      </h1>
-      <Image
-        src="/img/spinner.svg"
-        alt="Loading..."
-        width={100}
-        height={100}
-        className="w-20 h-20 mx-auto"
-      />
-    </main>
-  );
+  return <LoadingUI>Loading models...</LoadingUI>;
 }
---
+++ app/components/LoadingUI.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+export default function LoadingUI({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+      <h1 className="font-bold text-2xl text-orange-400 text-center mb-4">
+        {children}
+      </h1>
+      <Image
+        src="/img/spinner.svg"
+        alt="Loading..."
+        width={100}
+        height={100}
+        className="w-20 h-20 mx-auto"
+      />
+    </main>
+  );
+}
