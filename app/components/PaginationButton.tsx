9942b12b56ac28e10cfd1daaf0fda694e33b0fb1
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-20 17:53:45 +0800

    feat: update url using page query

--- app/components/PaginationButton.tsx
+++ app/components/PaginationButton.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 interface PaginationButtonProps {
   page: number;
 }
+
 export default function PaginationButton({ page }: PaginationButtonProps) {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  function handlePageChange() {
+    const urlSearchParams = new URLSearchParams(searchParams.toString());
+    urlSearchParams.set("page", page.toString());
+    const url = `${pathname}?${urlSearchParams.toString()}`;
+    router.push(url);
+  }
   return (
-    <button className="px-3 py-1.5 text-sm rounded-md border cursor-pointer border-gray-300 text-gray-700 hover:bg-gray-100">
+    <button onClick={handlePageChange} className="px-3 py-1.5 text-sm rounded-md border cursor-pointer border-gray-300 text-gray-700 hover:bg-gray-100">
       {page}
     </button>
   );
