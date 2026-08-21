1f53be029540a86eec6617533a406bf361db02b7
yy933 <116449560+yy933@users.noreply.github.com>
2026-08-21 16:36:13 +0800

    feat: render correct number of pagination buttons

--- app/components/PaginationControls.tsx
+++ app/components/PaginationControls.tsx
@@ -1,12 +1,16 @@
 import PaginationButton from "@/app/components/PaginationButton";
-export default function PaginationControls(totalPages: number) {
+export default function PaginationControls({
+  totalPages,
+}: {
+  totalPages: number;
+}) {
+  const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+  console.log(pages);
   return (
     <div className="flex justify-center gap-1">
-      <PaginationButton page={1} />
-      <PaginationButton page={2} />
-      <PaginationButton page={3} />
-      <PaginationButton page={4} />
-      <PaginationButton page={5} />
+      {pages.map((page) => (
+        <PaginationButton key={page} page={page} />
+      ))}
     </div>
   );
 }
